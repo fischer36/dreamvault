aa8c4770d66788be364548b3de2c4e3c2605e89c
Hampton33 <145158981+Hampton33@users.noreply.github.com>
2024-04-14 18:16:02 +0200

    Create config.h
    Placeholder config.h

---
+++ server/src/config.h
@@ -0,0 +1,15 @@
+// Data base auth and server configuration
+#ifdef WIN32
+#define DB_NAME "..."
+#define DB_USER "..."
+#define DB_HOST "..."
+#define DB_PASSWORD "..."
+#else
+#define DB_NAME "..."
+#define DB_USER "..."
+#define DB_HOST "..."
+#define DB_PASSWORD "..."
+#endif
+
+#define SERVER_IP "..."
+#define SERVER_PORT ...
