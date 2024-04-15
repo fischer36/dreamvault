d18cd01996a9996a89e9fbefb74b3e04d9ee1913
Hampton33 <145158981+Hampton33@users.noreply.github.com>
2024-04-15 02:30:44 +0200

    .gitignore update

--- server/src/config.h
+++ server/src/config.h
@@ -1,15 +1,17 @@
 // Data base auth and server configuration
 #ifdef WIN32
-#define DB_NAME "..."
-#define DB_USER "..."
-#define DB_HOST "..."
-#define DB_PASSWORD "..."
+#define DB_NAME "dreamvault"
+#define DB_USER "root"
+#define DB_HOST "localhost"
+#define DB_PORT 0
+#define DB_PASSWORD "hahaXD"
 #else
-#define DB_NAME "..."
-#define DB_USER "..."
-#define DB_HOST "..."
-#define DB_PASSWORD "..."
+#define DB_NAME "dreamvault"
+#define DB_USER "root"
+#define DB_HOST "localhost"
+#define DB_PORT 0
+#define DB_PASSWORD "hahaXD"
 #endif
 
 #define SERVER_IP "..."
-#define SERVER_PORT ...
+#define SERVER_PORT 8083
